817734d1445b804675f8e4dc917d7211c35d4611
Bruno Reginato <bruno.reginato@talabat.com>
2024-05-18 17:24:27 +0400

    Fixing 3.22.0 deprecated methods

--- lib/src/internals.dart
+++ lib/src/internals.dart
@@ -161,7 +161,7 @@ class Parser {
             case 'h1':
               double h1;
               if (defaultFontSize == null) {
-                h1 = Theme.of(context).textTheme.headline5?.fontSize ?? 24.0;
+                h1 = Theme.of(context).textTheme.headlineSmall?.fontSize ?? 24.0;
               } else {
                 h1 = defaultFontSize * 2;
               }
@@ -171,7 +171,7 @@ class Parser {
             case 'h2':
               double h2;
               if (defaultFontSize == null) {
-                h2 = Theme.of(context).textTheme.headline6?.fontSize ?? 20.0;
+                h2 = Theme.of(context).textTheme.titleLarge?.fontSize ?? 20.0;
               } else {
                 h2 = defaultFontSize * 1.5;
               }
@@ -181,7 +181,7 @@ class Parser {
             case 'h3':
               double h3;
               if (defaultFontSize == null) {
-                h3 = Theme.of(context).textTheme.subtitle1?.fontSize ?? 16.0;
+                h3 = Theme.of(context).textTheme.titleMedium?.fontSize ?? 16.0;
               } else {
                 h3 = defaultFontSize * 1.17;
               }
@@ -191,7 +191,7 @@ class Parser {
             case 'h4':
               double h4;
               if (defaultFontSize == null) {
-                h4 = Theme.of(context).textTheme.bodyText1?.fontSize ?? 16.0;
+                h4 = Theme.of(context).textTheme.bodyLarge?.fontSize ?? 16.0;
               } else {
                 h4 = defaultFontSize;
               }
@@ -201,7 +201,7 @@ class Parser {
             case 'h5':
               double h5;
               if (defaultFontSize == null) {
-                h5 = Theme.of(context).textTheme.bodyText1?.fontSize ?? 16.0;
+                h5 = Theme.of(context).textTheme.bodyLarge?.fontSize ?? 16.0;
               } else {
                 h5 = defaultFontSize * .83;
               }
@@ -211,7 +211,7 @@ class Parser {
             case 'h6':
               double h6;
               if (defaultFontSize == null) {
-                h6 = Theme.of(context).textTheme.bodyText2?.fontSize ?? 14.0;
+                h6 = Theme.of(context).textTheme.bodyMedium?.fontSize ?? 14.0;
               } else {
                 h6 = defaultFontSize * .67;
               }
